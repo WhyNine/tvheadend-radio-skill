2eff63d50e83e96fca030ced87525421cb4cc086
Simon Waller <waller_as@yahoo.co.uk>
2021-03-26 15:22:58 +0000

    fixed zero channels exception

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
+# Copyright Simon Waller
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from mycroft.util.log import getLogger
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 from mycroft.skills.audioservice import AudioService
@@ -20,6 +34,8 @@ class TVHeadendRadio(CommonPlaySkill):
         return self.regexes[regex]
 
     def CPS_match_query_phrase(self, phrase):
+        if len(self.channels) == 0:
+            return None
         match = re.search(self.translate_regex('on_tvheadend'), phrase)
         if match:
             data = re.sub(self.translate_regex('on_tvheadend'), '', phrase)

69107743a47be0c02b02c11186069f8426cfd876
Simon Waller <waller_as@yahoo.co.uk>
2021-04-10 12:17:29 +0100

    set vlc volume levels

--- __init__.py
+++ __init__.py
@@ -81,6 +81,8 @@ class TVHeadendRadio(CommonPlaySkill):
         self.backend = {}
         if "vlc" in backends.keys():
             self.backend["vlc"] = backends["vlc"]
+            self.backend["vlc"]["normal_volume"] = 80
+            self.backend["vlc"]["duck_volume"] = 5
             LOGGER.debug("Set vlc as backend to be used")
         self.regexes = {}
         

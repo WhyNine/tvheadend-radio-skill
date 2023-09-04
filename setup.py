7cbfdce6734fad8fd50af6fbaaecbda07adcd9f6
Simon Waller <55057479+WhyNine@users.noreply.github.com>
2023-09-04 20:48:17 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+   name='TvheadendRadio',
+   version='0.1',
+   packages=['requests','datetime','re'],
+   license='Apache 2.0',
+   long_description='Radio player using a TVheadend URL as the source',
+)

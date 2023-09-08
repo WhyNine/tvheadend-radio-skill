7d35f0ea6e9db2caa5b7a376e086b980faa71c35
Simon Waller <55057479+WhyNine@users.noreply.github.com>
2023-09-08 21:17:24 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup
+import os
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = f"{lib_folder}/requirements.txt"
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
    name='TvheadendRadio',
    version='0.1',
    packages=['tvheadend_radio_skill',],
+   install_requires=install_requires,
    license='Apache 2.0',
    long_description='Radio player using a TVheadend URL as the source',
 )

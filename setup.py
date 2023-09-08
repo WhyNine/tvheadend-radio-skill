9bfb8f7151a727a2db8dd29a7047d09792eadd76
Simon Waller <55057479+WhyNine@users.noreply.github.com>
2023-09-08 21:14:26 +0100

    change folder org

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
    name='TvheadendRadio',
    version='0.1',
-   packages=['datetime','re'],
+   packages=['tvheadend_radio_skill',],
    license='Apache 2.0',
    long_description='Radio player using a TVheadend URL as the source',
 )
